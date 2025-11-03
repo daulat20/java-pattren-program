144c88b81aefe2ad817852610171ab7b1d294913
Daulat Bhutal <daulatbhutal125@gmail.com>
2025-11-03 22:22:15 +0530

    Add files via upload

---
+++ RightTriangle.java
@@ -0,0 +1,14 @@
+package star_pattren;
+
+public class RightTriangle {
+	public static void main(String[] args) {
+	        int rows = 5;
+
+	        for (int i = 1; i <= rows; i++) { 
+	            for (int j = 1; j <= i; j++) { 
+	                System.out.print("* "); 
+	            }
+	            System.out.println(); 
+	        }
+	}
+}
